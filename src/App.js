b4c23891ddabaa96619677817c0f49fa0aaeb864
Pruthviraj Patil <95764252+pruthvirajp04@users.noreply.github.com>
2023-12-09 19:20:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [loginweb, setloginweb] = useState('');
+  const [passweb, setpassweb] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/api/login', {
+        loginweb,
+        passweb,
+      });
+
+      if (response.data.success) {
+        setMessage(response.data.message);
+        // Redirect to the home page or perform other actions upon successful login
+      } else {
+        setMessage(response.data.message);
+      }
+    } catch (error) {
+      console.error(error);
+      setMessage('An error occurred');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Login Page</h1>
+      <div>
+        <label>Login ID:</label>
+        <input type="text" value={loginweb} onChange={(e) => setloginweb(e.target.value)} />
+      </div>
+      <div>
+        <label>passweb:</label>
+        <input type="passweb" value={passweb} onChange={(e) => setpassweb(e.target.value)} />
+      </div>
+      <button onClick={handleLogin}>Login</button>
+      <p>{message}</p>
     </div>
   );
 }
