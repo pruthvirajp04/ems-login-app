d78ad44de1b0c9bb9e5e1f3ffa915e11add5d7a9
Pruthviraj Patil <pruthviraj.patil@goapptiv.in>
2023-12-12 17:25:38 +0530

    login design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 import axios from "axios";
 import {
-  MDBContainer,
-  MDBCol,
-  MDBRow,
   MDBBtn,
   MDBInput,
 } from "mdb-react-ui-kit";
+import './App.css'
 function App() {
   const [loginweb, setloginweb] = useState("");
   const [passweb, setpassweb] = useState("");
@@ -31,39 +29,48 @@ function App() {
   };
 
   return (
-    <MDBContainer fluid className="p-3 my-5">
-      <MDBRow>
-        <MDBCol col="10" md="6">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
-            className="img-fluid"
-          />
-        </MDBCol>
+    <>
+      <h1 className="mainTitle">
+        Welcome to <span> Coding Circle Academy</span>
+      </h1>
+      <h4 className="loginTitle">Login with your credentials🔑 </h4>
 
-        <MDBCol col="4" md="6">
-          <MDBInput
-            wrapperClass="mb-4"
-            label="Branch Name"
-            type="email"
-            size="lg"
-            value={loginweb}
-            onChange={(e) => setloginweb(e.target.value)}
-          />
-          <MDBInput
-            wrapperClass="mb-4"
-            label="Password"
-            type="password"
-            size="lg"
-            value={passweb}
-            onChange={(e) => setpassweb(e.target.value)}
-          />
+      <div fluid className="p-3 mainContainer">
+        <div className="subContainer">
+          <div col="" md="">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+              className="img-fluid"
+              height={500}
+              width={500}
+            />
+          </div>
 
-          <MDBBtn className="mb-4 w-100" size="lg" onClick={handleLogin}>
-            Sign in
-          </MDBBtn>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
+          <div col="" md="">
+            <MDBInput
+              wrapperClass="mb-4"
+              label="Branch Name"
+              type="email"
+              size="lg"
+              value={loginweb}
+              onChange={(e) => setloginweb(e.target.value)}
+            />
+            <MDBInput
+              wrapperClass="mb-4"
+              label="Password"
+              type="password"
+              size="lg"
+              value={passweb}
+              onChange={(e) => setpassweb(e.target.value)}
+            />
+
+            <MDBBtn className="mb-4 w-100" size="lg" onClick={handleLogin}>
+              Sign in
+            </MDBBtn>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
