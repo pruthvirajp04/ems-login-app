bde75bbf2313288f2b3c2b68b870e8474d0da23b
Pruthviraj Patil <pruthviraj.patil@goapptiv.in>
2023-12-17 18:55:11 +0530

    courses

--- src/components/CreateCourse/CreateCourse.js
+++ src/components/CreateCourse/CreateCourse.js
@@ -1,23 +1,54 @@
 import React from "react";
-import styled from "styled-components";
 import "./CreateCourse.css";
 import Navbar from "../Navbar/Navbar";
 import SideBar from "../SideBar/SideBar";
-
-const DashboardContainer = styled.div`
-  display: flex;
-`;
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const CreateCourse = () => {
   return (
     <>
       <Navbar />
-      <DashboardContainer>
+      <div className="mainBox">
         <SideBar />
         <div className="mainOmrBox">
-          <h1>CreateCourse Section</h1>
+          <h1>Course Registration Process</h1>
+          <div className="formBox">
+            <h3>Course Information</h3>
+            <form>
+              <Form.Control
+                type="text"
+                className="nameInput"
+                placeholder="Enter Course Name"
+              />
+              <Form.Control
+                type="text"
+                className="nameInput"
+                placeholder="Enter Course Description"
+              />
+              <Form.Control
+                type="text"
+                className="nameInput"
+                placeholder="Enter Course Duration"
+              />
+              <Form.Control
+                type="number"
+                className="nameInput"
+                placeholder="Enter fees"
+              />
+
+              <div className="btnBox">
+                <Button type="submit" variant="success">
+                  ADD COURSE
+                </Button>
+                <Button type="reset" variant="warning">
+                  RESET
+                </Button>
+              </div>
+            </form>
+          </div>
         </div>
-      </DashboardContainer>
+      </div>
     </>
   );
 };
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,29 +1,22 @@
 // src/Dashboard.js
 import React from "react";
-import styled from "styled-components";
 import "./Dashboard.css";
 import Navbar from "../Navbar/Navbar";
 import SideBar from "../SideBar/SideBar";
-const DashboardContainer = styled.div`
-  display: flex;
-  height: 100vh;
-`;
 
 
-const MainContent = styled.div`
-  flex: 1;
-  padding: 20px;
-`;
 const Dashboard = () => {
   return (
     <>
       <Navbar />
-      <DashboardContainer>
-      <SideBar />
-        <MainContent>
+      <div className="mainBox">
+        <SideBar />
+        <div className="mainOmrBox">
+
           <h1>Welcome to the Dashboard</h1>
-        </MainContent>
-      </DashboardContainer>
+        </div>
+      
+      </div>
     </>
   );
 };
